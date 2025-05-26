eb32adbd2462702105e416baf9a63c835ee90b13
Bicepsp <bicepspp@gmail.com>
2025-05-27 01:56:34 +0300

    Add build for GitHub Pages

---
+++ static/js/198.23ff94e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbestcoach_web=self.webpackChunkbestcoach_web||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.23ff94e0.chunk.js.map
